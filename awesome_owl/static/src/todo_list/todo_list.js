b87bbe1b4140e5df66dc0f34f719468f712a9402
Farhat Zafar <farhatzafar63@gmail.com>
2025-08-25 15:43:13 -0400

    create a todo list

---
+++ awesome_owl/static/src/todo_list/todo_list.js
@@ -0,0 +1,17 @@
+import { Component, useState } from "@odoo/owl";
+import { TodoItem } from "./todo_item";
+
+export class TodoList extends Component {
+
+    static template = "awesome_owl.todo_list";
+
+    static components = { TodoItem };
+
+    setup() {
+        this.todos = useState([
+            { id: 1, description: "first task", isCompleted: true}, 
+            { id: 2, description: "write tutorial", isCompleted: false},
+            { id: 3, description: "buy milk", isCompleted: false}
+        ]);
+    }
+}
