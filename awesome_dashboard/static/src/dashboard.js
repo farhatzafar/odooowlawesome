78f44d784f7471bd094d1b1751bc697ebc083fd9
Farhat Zafar <farhatzafar63@gmail.com>
2025-09-15 14:35:25 -0400

    add buttons linking to crm leads and contacts in dashboard

--- awesome_dashboard/static/src/dashboard.js
+++ awesome_dashboard/static/src/dashboard.js
@@ -2,6 +2,8 @@
 
 import { Component } from "@odoo/owl";
 import { registry } from "@web/core/registry";
+import { Layout } from "@web/search/layout";
+import { useService } from "@web/core/utils/hooks";
 
 class AwesomeDashboard extends Component {
     static template = "awesome_dashboard.AwesomeDashboard";
@@ -9,9 +11,26 @@ class AwesomeDashboard extends Component {
     static components = { Layout };
 
     setup() {
+        this.action = useService("Action");
         this.display = {
             controlPanel: {},
         };
+    }
+
+        openCustomerView() {
+        this.action.doAction("base.action_partner_form");
+    }
+
+    openLeads() {
+        this.action.doAction({
+            type: "ir.actions.act_window",
+            name: "All leads",
+            res_model: "crm.lead",
+            views: [
+                [false, "list"],
+                [false, "form"],
+            ],
+        });
     }
 }
 
