5f738400a8d41f73e8e6b9d1c81f78084b7323b6
Farhat Zafar <farhatzafar63@gmail.com>
2025-09-15 11:25:06 -0400

    add layout component, make background pink

--- awesome_dashboard/static/src/dashboard.js
+++ awesome_dashboard/static/src/dashboard.js
@@ -5,6 +5,14 @@ import { registry } from "@web/core/registry";
 
 class AwesomeDashboard extends Component {
     static template = "awesome_dashboard.AwesomeDashboard";
+
+    static components = { Layout };
+
+    setup() {
+        this.display = {
+            controlPanel: {},
+        };
+    }
 }
 
 registry.category("actions").add("awesome_dashboard.dashboard", AwesomeDashboard);
