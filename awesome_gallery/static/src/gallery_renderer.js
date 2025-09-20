5bd83fa7960623af0601d375a0c13b2621eeb185
Farhat Zafar <farhatzafar63@gmail.com>
2025-09-20 15:55:05 -0400

    reorder code into gallery model and gallery renderer

---
+++ awesome_gallery/static/src/gallery_renderer.js
@@ -0,0 +1,11 @@
+import { Component } from "@odoo/owl";
+import { GalleryModel } from "./gallery_model";
+
+export class GalleryRenderer extends Component {
+
+    static template = "awesome_gallery.GalleryRenderer";
+
+    static props = {
+        model: GalleryModel,
+    }
+}
